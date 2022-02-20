808d79e40ef68c999d1b67ea9bb6ea0147f8f531
MarikoMorimoto <mariko.morimoto0618@gmail.com>
2022-02-21 02:59:55 +0900

    Bootstrap4 へのダウングレードとそれに伴うlayouts.app を中心とした修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
+<!-- カルーセル -->
+<div class="container-fluid">
+    <div class="row w-100 no-gutters">
+        <div id="cl" class="carousel slide" data-ride="carousel" data-pause="false">
+            <ol class="carousel-indicators">
+                <li data-target="#cl" data-slide-to="0" class="active"></li>
+                <li data-target="#cl" data-slide-to="1"></li>
+                <li data-target="#cl" data-slide-to="2"></li>
+                <li data-target="#cl" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <a href="#">
+                        <img src="{{ asset('images/top-1.jpg') }}" class="d-block w-100" alt="">
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="{{ asset('images/top-2.jpg') }}" class="d-block w-100" alt="">
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="{{ asset('images/top-3.jpg') }}" class="d-block w-100" alt="">
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="{{ asset('images/top-4.jpg') }}" class="d-block w-100" alt="">
+                    </a>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#cl" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#cl" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </a>
+        </div>
+    </div>
+</div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
