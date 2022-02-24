97f3711cbedc2b79e06db7c82056e274f385a8ab
MarikoMorimoto <mariko.morimoto0618@gmail.com>
2022-02-25 03:12:50 +0900

    投稿の表示

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,39 @@
 
 @section('content')
 <div class="container">
-    <h2>仮ページ</h2>
+    <div class="row justify-content-center">
+        <div class="col-md-11 col-lg-8">
+            <h2>仮ページ</h2>
+            <div class="row text-center mt-2">
+                @forelse ($posts as $post)
+                    <div class="col-md-6 mt-3">
+                        @if ($post->image !== '')
+                        <img class="img-fluid" src="{{ asset('storage/' . $post->image) }}" alt="{{ $post->title }}">
+                        @else
+                        <img class="img-fluid" src="{{ asset('images/no_image.png') }}" alt="no_image">
+                        @endif
+                    </div>
+                    <div class="col-md-6 mt-3">
+                        <div class="mt-1">
+                            {{ $post->title }}
+                        </div>
+                        <div class="col-md-mt-3 text-left">
+                            {!! nl2br(e($post->comment)) !!}
+                        </div>
+                        <div class="py-2 text-right">
+                            {{ $post->created_at }}
+                        </div>
+                    </div>
+                    <div class="col-12 py-2 border-bottom"></div>
+                @empty
+                <p class="mt-3">
+                    投稿はありません。
+                </p>
+                @endforelse
+            </div>
+            {{ $posts->links() }}
+        </div>
+    </div>
 </div>
 
 @endsection
