96f6deb13558960ec28c356d54f0471635c2b7dd
MarikoMorimoto <mariko.morimoto0618@gmail.com>
2022-03-03 00:35:12 +0900

    非ログイン時のトップページ表示について修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,10 +29,17 @@ class HomeController extends Controller
         $categories = Category::all();
         // take() コレクションのメソッド 指定したアイテム数の新しいコレクションを返す
         $posts = Post::all()->sortByDesc('created_at')->take(6);
-        // ログインユーザーがフォローしているユーザーのid を取得
-        $follow_users_ids = \Auth::user()->follow_users->pluck('id');
-        // potsテーブルの中から user_id カラム が $follow_user_ids と一致するもののみ 3件 取得
-        $follow_users_posts = Post::all()->whereIn('user_id', $follow_users_ids)->sortByDesc('created_at')->take(6);
+        
+        // ユーザーがログインしていないときに Auth::user() は使えないため if 分岐させる
+        if (\Auth::user() !== null) {
+            // ログインユーザーがフォローしているユーザーのid を取得
+            $follow_users_ids = \Auth::user()->follow_users->pluck('id');
+            // potsテーブルの中から user_id カラム が $follow_user_ids と一致するもののみ 3件 取得
+            $follow_users_posts = Post::all()->whereIn('user_id', $follow_users_ids)->sortByDesc('created_at')->take(6);
+        } else {
+            // ユーザーがログインしていないときの処理 view に渡す変数は定義されている必要がある
+            $follow_users_posts = '';
+        }
         return view('home', [
             'categories' => $categories,
             'posts' => $posts,
